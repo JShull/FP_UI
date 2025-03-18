41e652536c4ce4fe2328ddade5a88c7e023111fb
John Shull <the.john.shull@gmail.com>
2025-03-18 11:37:53 -0400

    Canvas/Screen Bounds
    Drag working within a canvas/screen bounds config

--- Runtime/Scripts/FPUI_DragDropManager.cs
+++ Runtime/Scripts/FPUI_DragDropManager.cs
@@ -6,25 +6,38 @@ namespace FuzzPhyte.UI
     public class FPUI_DragDropManager : MonoBehaviour
     {
         public static FPUI_DragDropManager Instance { get; private set; }
-
-        // UI Events
+        public bool KeepInBounds;
+        [Tooltip("if we want to to manage it at the canvas level")]
+        public bool CanvasBounds;
+        [Tooltip("if we want to manage it at the screen level")]
+        public bool ScreenBounds;
+        // UI Events for listeners
         public UnityEvent<RectTransform> OnPickUp = new();
         public UnityEvent<RectTransform> OnDragging = new UnityEvent<RectTransform>();
-        public UnityEvent<RectTransform> OnDrop = new UnityEvent<RectTransform>();
         public UnityEvent<RectTransform> OnRelease = new UnityEvent<RectTransform>();
+        
+        [SerializeField]
+        protected RectTransform currentDragItem;
+        [SerializeField]
+        [Tooltip("Main Canvas of interest")]
+        protected Canvas parentCanvas;
+        protected Vector2 offset;
+        protected float pixelRadius = 10;
 
-        private RectTransform currentDragItem;
-        private Canvas parentCanvas;
-        private Vector2 offset;
-
-        void Awake()
+        public virtual void Awake()
         {
             if (Instance == null)
             {
                 Instance = this;
-                parentCanvas = GetComponentInParent<Canvas>();
                 if (parentCanvas == null)
-                    Debug.LogError("FPUI_DragDropManager needs to be under a Canvas.");
+                {
+                    parentCanvas = GetComponentInParent<Canvas>();
+                    if (parentCanvas == null)
+                    {
+                        Debug.LogError("FPUI_DragDropManager needs to be under a Canvas.");
+                    }
+                }
+
             }
             else
             {
@@ -32,7 +45,7 @@ namespace FuzzPhyte.UI
             }
         }
 
-        void Update()
+        public virtual void Update()
         {
             if (currentDragItem != null)
             {
@@ -43,22 +56,74 @@ namespace FuzzPhyte.UI
                     parentCanvas.worldCamera,
                     out movePos);
                 currentDragItem.position = parentCanvas.transform.TransformPoint(movePos - offset);
-                OnDragging.Invoke(currentDragItem);
+                
+                OnDragging?.Invoke(currentDragItem);
+                //check if we are out of the screen bounds
+                if (KeepInBounds)
+                {
+                    BoundsCheck();
+                }
+                
             }
         }
+        protected virtual void BoundsCheck()
+        {
+            ///establish corners given current canvas size - it might be live adusting so we need to keep getting this
+            Vector2 lowerBounds=Vector2.zero;
+            Vector2 upperBounds=Vector2.zero;
+            Vector2 areaCenter=Vector2.zero;
+            Vector2 currentPosition = currentDragItem.position;
 
-        public void BeginDrag(RectTransform item, Vector2 pointerOffset)
+            if (CanvasBounds)
+            {
+                RectTransform canvasRectTransform = parentCanvas.transform as RectTransform;
+                Vector3[] canvasCorners = new Vector3[4];
+                canvasRectTransform.GetWorldCorners(canvasCorners);
+                lowerBounds = canvasCorners[0];
+                upperBounds = canvasCorners[2];
+                areaCenter = (lowerBounds + upperBounds) / 2f;
+            }
+            if (ScreenBounds)
+            {
+                lowerBounds = new Vector2(0, 0);
+                upperBounds = new Vector2(Screen.width, Screen.height);
+                areaCenter = new Vector2(Screen.width / 2, Screen.height / 2);
+            }
+            
+
+            if (currentDragItem.position.x < lowerBounds.x || currentDragItem.position.y < lowerBounds.y ||
+                    currentDragItem.position.x > upperBounds.x || currentDragItem.position.y > upperBounds.y)
+            {
+
+                //bump it back out towards the center of the canvas by some amount
+                //2D vector back to center to calculate direction to move along 
+                Vector2 direction = (currentPosition - areaCenter).normalized;
+
+
+                // Move the item back towards the center by a fixed amount of pixels
+                Vector2 newPosition = currentPosition - direction * pixelRadius;
+
+                // Ensure the new position is within screen bounds
+                newPosition.x = Mathf.Clamp(newPosition.x, lowerBounds.x, upperBounds.x);
+                newPosition.y = Mathf.Clamp(newPosition.y, lowerBounds.x, upperBounds.y);
+                currentDragItem.position = newPosition;
+                
+                EndDrag();
+            }
+        }
+        public void BeginDrag(RectTransform item, Vector2 pointerOffset, float pixelSize=50)
         {
             currentDragItem = item;
+            pixelRadius = pixelSize;
             offset = pointerOffset;
-            OnPickUp.Invoke(item);
+            OnPickUp?.Invoke(item);
         }
 
         public void EndDrag()
         {
             if (currentDragItem != null)
             {
-                OnRelease.Invoke(currentDragItem);
+                OnRelease?.Invoke(currentDragItem);
                 currentDragItem = null;
             }
         }
--- Runtime/Scripts/FPUI_DragItem.cs
+++ Runtime/Scripts/FPUI_DragItem.cs
@@ -1,19 +1,77 @@
-using UnityEngine;
-
 namespace FuzzPhyte.UI
 {
-    public class FPUI_DragItem : MonoBehaviour
+    using UnityEngine;
+    using UnityEngine.EventSystems;
+
+    public class FPUI_DragItem : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
+        [SerializeField]
+        protected RectTransform rectTransform;
+        [SerializeField]
+        protected FPUI_Moveable moveable;
+        [SerializeField]
+        protected bool error=false;
+
+        void Awake()
         {
-        
+            if (moveable == null)
+            {
+                moveable = GetComponent<FPUI_Moveable>();
+                if (moveable == null)
+                {
+                    Debug.LogError("FPUI_DragItem needs a FPUI_Moveable component.");
+                    error = true;
+                    return;
+                }
+                else
+                {
+                    error = false;
+                }
+            } else
+            {
+                error = false;
+            }
+            //now check our recttransform
+            if (rectTransform == null)
+            {
+                rectTransform = GetComponent<RectTransform>();
+                if (rectTransform == null)
+                {
+                    Debug.LogError("FPUI_DragItem needs a RectTransform.");
+                    error = true;
+                    return;
+                }
+                else
+                {
+                    error = false;
+                }
+            }
+            else
+            {
+                error = false;
+            }
         }
 
-        // Update is called once per frame
-        void Update()
+        public void OnPointerDown(PointerEventData eventData)
         {
-        
+            if (error) return;
+            Vector2 localPointerPosition;
+            Debug.Log($"On Pointer Down!");
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                rectTransform,
+                eventData.position,
+                eventData.pressEventCamera,
+                out localPointerPosition);
+            //get my width of the rect transform in pixels
+            var width = rectTransform.rect.width;
+            FPUI_DragDropManager.Instance.BeginDrag(rectTransform, localPointerPosition, width);
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            Debug.Log($"On pointer up");
+            if(error) return;
+            FPUI_DragDropManager.Instance.EndDrag();
         }
     }
 }
